b63b1617b0ca42dc7fea0709355209d4cbf2c283
4r36 <rodrigocostamoura@hotmail.com>
2021-11-04 12:35:05 -0100

    imagem e style

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 app.use(express.static('./public'))
 
-app.get('/', (req, res)=>{
-    res.send('Hello World')
+
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'./public/index.html'))
+})
+
+/*app.get('/', (req, res)=>{
+  res.send('Hello World')
   }) 
 
 app.get('/:nome', (req, res)=>{
   res.send('Hello '+ req.params.nome)
-})
+})  */
 
 const port = 3000
  
